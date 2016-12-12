9018176ac88fa2ad052eaaf53e9c94b4a9d237f1
ricsirke <benericsirke@gmail.com>
2016-12-12 17:49:50 +0100

    1d: dimensions fixed

--- play/1d.py
+++ play/1d.py
@@ -5,19 +5,21 @@
 # u(1) = 0
 # f(x) = -2
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
 np.set_printoptions(precision=3)
 print "\n"
 
-nx = 10
+nx = int(sys.argv[1])
 xmin = 0.0
 xmax = 1.0
 
 h = (xmax-xmin)/nx
 
 nodes = np.array([xmin + k*h for k in range(nx+1)])[1:-1]
+N = len(nodes)
 print "nodes:", nodes, "\n"
 
 def source_fn(x):
@@ -33,23 +35,25 @@ u_pont = np.array([ u_pont(nd) for nd in nodes ])
 grPhi1 = nx
 grPhi2 = -nx
 
-A = np.zeros((nx-1,nx-1))
+A = np.zeros((N, N))
 
-# looping thrugh the elements
+# looping through the elements
+
 A[0][0] += grPhi1*grPhi1*h
 A[-1][-1] += grPhi2*grPhi2*h
 
-for i in range(1, nx-1):
+for i in range(1, N):
     A[i][i] += grPhi2*grPhi2*h
     A[i][i-1] += grPhi1*grPhi2*h
     A[i-1][i-1] += grPhi1*grPhi1*h
     A[i-1][i] += grPhi2*grPhi1*h
+
     
 #print A
 print "A:\n", A, "\n"
 
 # b
-b = np.array([ h*f[i] for i in range(nx-1) ])
+b = np.array([ h*f[i] for i in range(N) ])
 print "b:\n", b, "\n"
     
 
@@ -63,8 +67,8 @@ print "pont_sol:\n", u_pont, "\n"
 # errors
 errors = np.array([ np.abs(u[i] - u_pont[i]) for i in range(len(nodes)) ])
 print "errors:", errors, "\n"
-print "error sum:", sum(errors)
+print "error max:", np.max(errors)
 
 
-plt.plot(u, 'r')
+plt.plot(u, 'ro', u_pont, 'b')
 plt.show()
